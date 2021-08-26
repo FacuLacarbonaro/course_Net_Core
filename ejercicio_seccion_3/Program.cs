59b3717ed448a87926fd284b499bb432bd03dcea
Cufa <Cufa@DESKTOP-CKNNHC7>
2021-08-26 13:18:42 -0300

    Add project files.

---
+++ ejercicio_seccion_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ejercicio_seccion_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce tu nombre:");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hola " + name);
+
+            Console.ReadKey();
+
+
+            Console.WriteLine("Introduce una ciudad:");
+
+            string city = Console.ReadLine();
+
+            Console.WriteLine("Hola " + name + " bienvenido a " + city);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
